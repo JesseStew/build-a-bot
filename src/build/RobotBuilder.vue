2e6f50cc47158733504ed54a37143e3bb489f8b1
Jesse Stewart <stewed.jesse@gmail.com>
2019-10-31 12:30:54 -0500

    Conditionally Applying Classes with Class Bindings (part 2)

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part" :class="{'sale-border': selectedRobot.head.onSale}">
+      <div :class="[saleBorderClass, 'top', 'part']">
         <div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-if="selectedRobot.head.onSale" class="sale">
@@ -85,6 +85,9 @@ export default {
     }
   },
   computed: {
+    saleBorderClass() {
+      return this.selectedRobot.head.onSale ? 'sale-border' : ''
+    },
     headBorderStyle() {
       return {
         border: this.selectedRobot.head.onSale ? '3px solid red' : '3px solid #aaa'
