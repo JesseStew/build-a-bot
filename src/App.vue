a9c034ffb1e8ce85eb757a90532d984285b8079d
Jesse Stewart <stewed.jesse@gmail.com>
2019-11-03 16:05:17 -0600

    Linking to Routed Pages

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img src="./assets/build-a-bot-logo.png" alt="" class="logo">
-            Build-a-Bot
+            <router-link class="nav-link" :to="{name: 'Home'}">
+              <img src="./assets/build-a-bot-logo.png" alt="" class="logo">
+              Build-a-Bot
+            </router-link>
           </li>
         </ul>
       </nav>
@@ -60,5 +62,9 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
