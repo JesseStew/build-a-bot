ee5d980dd499025127016f7af6809687fc1e04e3
Jesse Stewart <stewed.jesse@gmail.com>
2019-11-03 17:00:49 -0600

    Working with Route Params

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -2,21 +2,21 @@
   <div>
     <h1>{{ part.title }}</h1>
     <div>
-      {{part.description}}
+      {{ part.description }}
     </div>
   </div>
 </template>
 
 <script>
+import parts from '../data/parts'
+
 export default {
   name: 'PartInfo',
-  data() {
-    return {
-      part: {
-        title: 'Part Title',
-        description: 'Part Description',
-      },
+  computed: {
+    part() {
+      const { partType, id } = this.$route.params
+      return parts[partType].find(part => part.id === +id) // + casts to number
     }
-  }
+  },
 }
 </script>
