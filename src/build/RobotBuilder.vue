70f13caec9fab7824d62da7a809dced577aa0f46
Jesse Stewart <stewed.jesse@gmail.com>
2019-11-12 10:20:06 -0600

    Using the Vuex MapActions Helper

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -60,6 +60,8 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 import createdHookMixin from './create-hook-mixin'
 import PartSelector from './PartSelector.vue'
 import CollapsibleSection from '../shared/CollapsibleSection.vue'
@@ -67,7 +69,7 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue'
 export default {
   name: 'RobotBuilder',
   created() {
-    this.$store.dispatch('robots/getParts')
+    this.getParts()
   },
   beforeRouteLeave(to, from , next) {
     if (this.addedToCart) {
@@ -106,6 +108,7 @@ export default {
     },
   },
   methods: {
+    ...mapActions('robots', ['getParts', 'addRobotToCart']),
     addToCart() {
       const robot = this.selectedRobot
       const cost = robot.head.cost +
@@ -113,7 +116,7 @@ export default {
         robot.torso.cost +
         robot.rightArm.cost +
         robot.base.cost
-      this.$store.dispatch('robots/addRobotToCart', Object.assign({}, robot, { cost }))
+      this.addRobotToCart(Object.assign({}, robot, { cost }))
         .then(() => this.$router.push('/cart'))
       this.addedToCart = true
     },
