429cf87b32ad3d0a27f07a6e132a09ed1910e879
Jesse Stewart <stewed.jesse@gmail.com>
2019-11-14 12:49:23 -0600

    Customizing Webpack Config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,29 @@
 module.exports = {
+  configureWebpack: (config) => {
+    config.modules.rules.push({
+      test: /\.coffee$/,
+      use: ['coffee-loader']
+    })
+
+    // Could also use webpack chain to modify config (SAFER)
+    const newRule = {
+      test:/\.(png|jp?g|gif)(\?.*)?$/,
+      use: [
+        {
+          loader: 'url-loader',
+          options: {
+            limit: 5000,
+            name: 'img/[name].[hash:8].[ext]',
+          }
+        }
+      ]
+    }
+
+    const imagesRuleIndex = config.module.rules
+      .findIndex(x => x.test.source.includes('png|jp?g|gif'))
+
+    config.module.rules.splice(imagesRuleIndex, 1, newRule)
+  },
   devServer: {
     proxy: {
       '/api': {
